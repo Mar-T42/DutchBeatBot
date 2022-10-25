8cab1abe852b7dcbd2aed8b32f3a2ff96524343e
MarT <luycksmartijn@gmail.com>
2022-10-25 21:23:13 +0200

    Attempt 2

--- bot.js
+++ bot.js
@@ -9,22 +9,22 @@ Client.config({
 const BOT_PREFIX = '/';
 const BOT_PING_COMMAND = 'ping';
 
-Client.on('ready', () => {
+client.on('ready', () => {
   console.log(`Logged in as ${Client.user.tag}!.`);
   console.log(`Our Kitchen 3.0 DutchBeatBot is alive AND kicking!!`);
 });
 
-Client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
     replyPongWhenPingEntered(interaction);
 });
 
-Client.on('messageDelete', msg => {
+client.on('messageDelete', msg => {
         msg.channel.send('Eh.. stop deleting messages please! History is history..');
     }
 );
 
-Client.on("message", message => {
+client.on("message", message => {
     if (message.content.toLowercase() === 'i love dutchbeat') {
         message.react('❤️');
     }
