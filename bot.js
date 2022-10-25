a3d23aa069e367b95ed2efcd780255ccb8d5c24e
MarT <luycksmartijn@gmail.com>
2022-10-25 20:34:20 +0200

    DutchBeatBot 1st tests

---
+++ bot.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ 
+    intents: [GatewayIntentBits.Guilds] ,
+    partials:  [`MESSAGE`]
+});
+
+const BOT_PREFIX = '/';
+const BOT_PING_COMMAND = 'ping';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!.`);
+  console.log(`Our Kitchen 3.0 DutchBeatBot is alive AND kicking!!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+    replyPongWhenPingEntered(interaction);
+});
+
+client.on('messageDelete', msg => {
+        msg.channel.send('Eh.. stop deleting messages please! History is history..');
+    }
+);
+
+client.on("message", message => {
+    if (message.content.toLowercase() === 'i love dutchbeat') {
+        message.react('❤️');
+    }
+})
+
+const replyPongWhenPingEntered = async (interaction) => {
+    if (interaction.commandName === `${BOT_PREFIX}${BOT_PING_COMMAND}`) {ProcessingInstructionp
+        await interaction.reply('Pong!');
+      }
+}
+
+client.login(process.env.BOT_TOKEN);
